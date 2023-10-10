ded3e6ca74f0083fa936be0c6ae6be364b2521ed
Tuanesfreitas <tuanesfreitasab@gmail.com>
2023-10-10 20:38:55 -0300

    adicionando a image background

--- Pages/Login.js
+++ Pages/Login.js
@@ -1,27 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
 import Input from '../Components/Input'
 import Button from '../Components/Button'
-
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ImageBackground } from 'react-native'
 import { Formik } from 'formik'
-import {loginValidationsSchema}from '../validations/Form'
+import { loginValidationsSchema } from '../validations/Form'
+
+const image = require('../assets/images/img6.jpeg')
 
 export default function Login({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <View style={styles.containerWrapper}>
         <Formik validationSchema={loginValidationsSchema} initialValues={{ Email: '', Password: '' }} onSubmit={(values) => navigation.navigate('Home')}>
           {
             ({
               handleChange,
               handleSubmit,
-              values, 
+              values,
               errors,
               isValid
             }) => (<>
               <View style={styles.containerInput}>
                 <Input name="Email" onChange={handleChange('Email')} value={values.Email} type="text" placeholder="Email" />
-                {errors.Email && <Text style={styles.textError} >{errors.Email}</Text>}
+                {errors.Email && <Text
+                  style={styles.textError} >{errors.Email}</Text>}
               </View>
               <View style={styles.containerInput}>
                 <Input name="Password" onChange={handleChange('Password')} value={values.Password} type="password" placeholder="Senha" />
@@ -39,22 +42,24 @@ export default function Login({ navigation }) {
           <Text style={styles.containerResetPasswordLinkContentText}>Esqueceu sua senha?</Text>
         </TouchableOpacity>
       </View>
+      </ImageBackground>
     </SafeAreaView>
+ 
   )
-}
+        }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: '3%',
-    paddingLeft: '5%',
-    paddingRight: '5%',
     gap: 30
   },
   containerWrapper: {
     flex: 1,
     width: '100%',
+    paddingLeft: '5%',
+    paddingRight: '5%'
   },
   containerInput: {
     paddingTop: 18,
@@ -75,8 +80,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
   },
-  textError:{
-color:'red'
-
+  textError: {
+    color: 'red'
+  },
+  image: {
+    flex: 1,
   }
 })
